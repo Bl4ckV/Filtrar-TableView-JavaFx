2e87c18e2479ef3635f8a669448a72aa65ca15b4
Álvaro R <104674473+Bl4ckV@users.noreply.github.com>
2022-09-30 12:45:18 +0200

    Add files via upload

---
+++ Filtrar-TableView.java
@@ -0,0 +1,26 @@
+@FXML
+private ObservableList<Pedido> pedidos;
+private ObservableList<Pedido> pedidos1 = FXCollections.observableArrayList();
+
+@FXML
+public void eliminarPedido() {
+    tablaPedidos.getItems().remove(tablaPedidos.getSelectionModel().getSelectedItem());
+    for (int i = 0; i < pedidos.size(); i++) {
+        if (pedidos.get(i).getTipo().equalsIgnoreCase(eFiltrar.getText()) || pedidos.get(i).getTipo().equalsIgnoreCase(eFiltrar.getText()))
+            pedidos.remove(i);
+    }
+}
+
+public void buscar() {
+    if (eFiltrar.getText().equalsIgnoreCase("")) {
+                tablaPedidos.setItems((ObservableList)pedidos);
+    }
+    else {
+        pedidos1.clear();
+        for (int i = 0; i < pedidos.size() ; i++) {
+            if (pedidos.get(i).getTipo().equalsIgnoreCase(eFiltrar.getText())) 
+               pedidos1.add(pedidos.get(i));
+        } 
+        tablaPedidos.setItems((ObservableList)pedidos1);
+    }
+}
